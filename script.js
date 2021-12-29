6684d7aa05da50f37d05ae21f7cbc253e5906e81
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2021-12-29 13:32:26 +0600

    10 examples

---
+++ script.js
@@ -0,0 +1,76 @@
+function teacherOfEnglish(name) {
+    console.log(name, "checkHomeWork");
+    console.log(name,"explainLessons");
+    console.log(name,"correctPaper");
+    console.log(name,"orderBooks");
+}
+teacherOfEnglish("Altynai")
+teacherOfEnglish("Emirlan")
+teacherOfEnglish("BAitemir")
+
+
+function guestOfHotel(name) {
+    console.log(name, "bookTheRoom");
+    console.log(name, "checkIn");
+    console.log(name, "paidForServices");
+    console.log(name, "checkOut");
+}
+guestOfHotel("Tagaytay")
+guestOfHotel("Karagat")
+guestOfHotel("Amir")
+guestOfHotel("Altamira")
+
+function test(a){
+    a = a * 2
+    return a
+}
+console.log(test(4));
+function test(){
+    if(){
+
+    }
+    else{
+
+    }
+}
+
+function multiple(a) {
+    return a  = a * 3.14 
+}
+console.log(multiple(10));
+
+
+function addition(number) {
+    return number = number + 999
+}
+console.log(addition(867));
+
+function countdown() {
+    for (let i = 100; i < 1000; i++) {
+        document.write(i + "<br>")
+
+    }
+}
+
+function colorful() {
+    var red = Math.random() * 255;
+    var blue = Math.random() * 255;
+    var green = Math.random() * 255;
+    document.write('<h1 style="color: rgb(' + red +',' + green + ',' + blue + ');">Hello World</h1>');
+}
+colorful();
+colorful();
+colorful();
+colorful();
+
+function colorful() {
+    var yellow = 100;
+    var pink = 100;
+    var black = 100;
+    document.write('<h1>Karakol</h1> <br> <h2>Welcome to Karakol</h2> <p>Karakol ski base is located 400 km from the capital of Kyrgyzstan and situated at altitude of 3040 meters above sea level on the slopes of the Tien Shan mountains. This is the highest ski resort in Central Asia. It is located approximately 30 minutes and 7 km from Karakol city.<p/>')
+}
+colorful();
+colorful();
+colorful();
+
+
